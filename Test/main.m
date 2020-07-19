54ab7308609b631ae7b259589d1e12db4bc41d43
yaoyan <yaoyanedu@163.com>
2020-07-19 11:00:26 +0800

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Yan on 2020/7/19.
+//  Copyright © 2020年 Yan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
